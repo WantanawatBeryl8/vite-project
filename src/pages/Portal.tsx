24850f595afb3b530bf33414693c38fdab1e2c6b
Wantanawat Jitprakop <wantanawat.j@extend-it-resource.com>
2024-10-29 11:59:09 +0700

    `feat:` user Storage

---
+++ src/pages/Portal.tsx
@@ -0,0 +1,13 @@
+// import { useNavigate } from 'react-router-dom';
+
+function PortalPage() {
+  // const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Welcome to the Portal</h1>
+      <p>This is the portal page.</p>
+    </div>
+  );
+}
+
+export default PortalPage;
