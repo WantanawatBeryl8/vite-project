9f1e36de950b3958c07ca441aac50a3f9fc7888d
Wantanawat Jitprakop <wantanawat.j@extend-it-resource.com>
2024-10-29 11:10:43 +0700

    `feat:` add site map

---
+++ src/core/utils/sitemap.ts
@@ -0,0 +1,23 @@
+export const siteMap = {
+  portal: '/portal',
+  accountPlanning: {
+    callPlanCallMemo: '/account-planning/call',
+    smeBUsiness: '/account-planning/sme',
+    boAndFamily: '/account-planning/bo-family',
+    employee: '/account-planning/employee',
+    supplyChain: '/account-planning/supply-chain',
+  },
+  customerInformation: {
+    customerOverview: '/customer-information/overview',
+    customerInformation: '/customer-information/information',
+    treeView: '/customer-information/tree-view',
+    boAndFamily: '/customer-information/bo-family',
+    employee: '/customer-information/employee',
+  },
+  report: {
+    index: '/report/',
+  },
+  adminMenu: {
+    reportConfiguration: '/admin-menu/report',
+  },
+};
