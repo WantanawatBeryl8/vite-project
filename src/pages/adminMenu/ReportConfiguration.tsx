ad200e83c216cb230fdfd3607d9389c9231c2b4b
pakorn-X10 <pakorn.k@extend-it-resource.com>
2024-10-29 15:11:47 +0700

    `feat`: Create component pages and add routes for Admin menu module

---
+++ src/pages/adminMenu/ReportConfiguration.tsx
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
+function ReportConfigurationPage() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Report Configuration</h1>
+    </div>
+  );
+}
+
+export default ReportConfigurationPage;
