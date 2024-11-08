eb83ea30dd51c9962106076ab46725f470d8ebd8
Peemasit Punpuang <peemasit.p@extend-it-resource.com>
2024-11-08 12:55:57 +0700

    `fix`: delete components ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,16 +47,6 @@ export default {
         hover: {
           'light-purple': '#C8ADE6',
         },
-        sidebar: {
-          DEFAULT: 'hsl(var(--sidebar-background))',
-          foreground: 'hsl(var(--sidebar-foreground))',
-          primary: 'hsl(var(--sidebar-primary))',
-          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-          accent: 'hsl(var(--sidebar-accent))',
-          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-          border: 'hsl(var(--sidebar-border))',
-          ring: 'hsl(var(--sidebar-ring))',
-        },
       },
       keyframes: {
         'accordion-down': {
